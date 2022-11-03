37e7a9fd744bbfbf0e9e57097a4ea6034b2d2f59
Jqwong <59355564+Jquil@users.noreply.github.com>
2022-11-03 19:09:10 +0800

    Add files via upload

---
+++ IBaseReflectHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace ESP.Expand.Reflect.Interface
+{
+    public interface IBaseReflectHelper
+    {
+
+        /// <summary>
+        /// 对象转化
+        /// </summary>
+        /// <typeparam name="T1">待转化对象</typeparam>
+        /// <typeparam name="T2">数据源</typeparam>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        T1 ObjectTransformation<T1, T2>(T2 root) where T1 : new();
+
+
+
+        /// <summary>
+        /// DataTable转化为List
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="dt"></param>
+        /// <returns></returns>
+        List<T> DataTableToList<T>(DataTable dt);
+
+
+
+        /// <summary>
+        /// List转为为DataTable
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        DataTable ListToDataTable<T>(List<T> data);
+    }
+}
