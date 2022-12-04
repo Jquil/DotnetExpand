ed3fcbb5c808b08ff889bd079f6a320fab9bfdfa
Jq <835292266@qq.com>
2022-12-04 16:31:23 +0800

    init log

---
+++ Log/Log.cs
@@ -0,0 +1,36 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expand.Log
+{
+    public class Log
+    {
+
+
+        /// <summary>
+        /// 信息日志
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="msg"></param>
+        public static void Info(string key,string msg) {
+            var logger = LogManager.GetLogger(key);
+            logger.Info(msg);
+        }
+
+
+
+        /// <summary>
+        /// 错误日志
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="msg"></param>
+        public static void Error(string key,string msg) {
+            var logger = LogManager.GetLogger(key);
+            logger.Error(msg);
+        }
+    }
+}
